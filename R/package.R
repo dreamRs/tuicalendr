01d62b21aad18a524def5236dbeda7531e6796e1
pvictor <perrier.victor@gmail.com>
2019-04-23 15:16:43 +0200

    import/export magrittr

---
+++ R/package.R
@@ -0,0 +1,25 @@
+#' An \code{htmlwidget} interface to the
+#' tui-calendar javascript library
+#' 
+#' This package allow you to use ApexCharts.js (\url{https://github.com/nhn/tui.calendar}),
+#' to create interactive calendars.
+#' 
+#' @name tui.calendar-package
+#' @docType package
+#' @author Victor Perrier (@@dreamRs_fr)
+NULL
+
+#' tui.calendar exported operators and S3 methods
+#' 
+#' The following functions are imported and then re-exported
+#' from the tui.calendar package to avoid listing the magrittr
+#' as Depends of tui.calendar
+#' 
+#' @name tui.calendar-exports
+NULL
+
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @export
+#' @rdname tui.calendar-exports
+NULL
