37baeef2a1c13cf5142048915abc2206188aaa87
pvictor <perrier.victor@gmail.com>
2020-03-23 18:18:48 +0100

    bug button nav eventlistener duplicate

--- inst/htmlwidgets/calendar.js
+++ inst/htmlwidgets/calendar.js
@@ -1,31 +1,27 @@
 HTMLWidgets.widget({
+  name: "calendar",
 
-  name: 'calendar',
-
-  type: 'output',
+  type: "output",
 
   factory: function(el, width, height) {
-
     var Calendar = tui.Calendar;
-    var cal;
+    var cal, renderRange;
 
     return {
-
       renderValue: function(x) {
-        
         var menu = document.getElementById(el.id + "_menu");
-        
+
         if (!x.useNav) {
           if (menu !== null) {
             menu.parentNode.removeChild(menu);
           }
         }
-        
+
         if (typeof cal !== "undefined") {
           cal.destroy();
           el.innerHTML = "";
         }
-        
+
         var options = x.options;
 
         cal = new Calendar(el, options);
@@ -34,32 +30,36 @@ HTMLWidgets.widget({
         if (x.hasOwnProperty("defaultDate")) {
           cal.setDate(x.defaultDate);
         }
-        
-        // nav
+
+        // Navigation button
         if (x.useNav) {
-          var renderRange = document.getElementById(el.id + "_renderRange");
-          renderRange.innerHTML = dateToYMD(cal.getDateRangeStart()) + " - " + dateToYMD(cal.getDateRangeEnd());
+          renderRange = document.getElementById(el.id + "_renderRange");
+          renderRange.innerHTML =
+            dateToYMD(cal.getDateRangeStart()) +
+            " - " +
+            dateToYMD(cal.getDateRangeEnd());
+
           var prev = document.getElementById(el.id + "_prev");
           prev.className += x.bttnOpts.class;
           prev.innerHTML = x.bttnOpts.prev_label;
-          prev.addEventListener("click", function(e) {
-            cal.prev();
-            renderRange.innerHTML = dateToYMD(cal.getDateRangeStart()) + " - " + dateToYMD(cal.getDateRangeEnd());
-          }, false);
+          //var clickPrevEvent = clickPrev(cal, renderRange);
+          prev.removeEventListener("click", this.clickPrev);
+          prev.addEventListener("click", this.clickPrev);
+
           var next = document.getElementById(el.id + "_next");
           next.className += x.bttnOpts.class;
           next.innerHTML = x.bttnOpts.next_label;
-          next.addEventListener("click", function(e) {
-            cal.next();
-            renderRange.innerHTML = dateToYMD(cal.getDateRangeStart()) + " - " + dateToYMD(cal.getDateRangeEnd());
-          }, false);
+          //var clickNextEvent = clickNext(cal, renderRange);
+          next.removeEventListener("click", this.clickNext);
+          next.addEventListener("click", this.clickNext);
+
           var today = document.getElementById(el.id + "_today");
           today.className += x.bttnOpts.class;
           today.innerHTML = x.bttnOpts.today_label;
-          today.addEventListener("click", function(e) {
-            cal.today();
-            renderRange.innerHTML = dateToYMD(cal.getDateRangeStart()) + " - " + dateToYMD(cal.getDateRangeEnd());
-          }, false);
+          //var clickTodayEvent = clickToday(cal, renderRange);
+          today.removeEventListener("click", this.clickToday);
+          today.addEventListener("click", this.clickToday);
+
           if (x.bttnOpts.hasOwnProperty("bg")) {
             prev.style.background = x.bttnOpts.bg;
             next.style.background = x.bttnOpts.bg;
@@ -72,187 +72,199 @@ HTMLWidgets.widget({
           }
         }
 
-        
         // shiny input
         if (HTMLWidgets.shinyMode) {
-          Shiny.setInputValue(el.id + '_dates', {
+          Shiny.setInputValue(el.id + "_dates", {
             current: moment(cal.getDate()._date).format(),
             start: moment(cal.getDateRangeStart()._date).format(),
             end: moment(cal.getDateRangeEnd()._date).format()
           });
-          
-          if (x.events.hasOwnProperty('beforeCreateSchedule')) {
-            cal.on('beforeCreateSchedule', x.events.beforeCreateSchedule);
+
+          if (x.events.hasOwnProperty("beforeCreateSchedule")) {
+            cal.on("beforeCreateSchedule", x.events.beforeCreateSchedule);
           } else {
-            cal.on('beforeCreateSchedule', function(event) {
+            cal.on("beforeCreateSchedule", function(event) {
               //console.log(event);
-              Shiny.setInputValue(el.id + '_add_schedule', {
+              Shiny.setInputValue(el.id + "_add_schedule", {
                 title: event.title,
                 location: event.location,
                 start: moment(event.start._date).format(),
                 end: moment(event.end._date).format(),
                 isAllDay: event.isAllDay,
-                category: event.isAllDay ? 'allday' : 'time',
-                calendarId: event.calendarId,
+                category: event.isAllDay ? "allday" : "time",
+                calendarId: event.calendarId
               });
             });
           }
-          
-          if (x.events.hasOwnProperty('afterRenderSchedule')) {
-            cal.on('afterRenderSchedule', x.events.afterRenderSchedule);
+
+          if (x.events.hasOwnProperty("afterRenderSchedule")) {
+            cal.on("afterRenderSchedule", x.events.afterRenderSchedule);
           } else {
-            cal.on('afterRenderSchedule', function(event) {
+            cal.on("afterRenderSchedule", function(event) {
               //var shedule = cal.getSchedule();
               var schedule = event.schedule;
               var element = cal.getSchedule(schedule.id, schedule.calendarId);
-              Shiny.setInputValue(el.id + '_schedules', element);
-            });
-          }
-          
-          if (x.events.hasOwnProperty('clickSchedule')) {
-            cal.on('clickSchedule', x.events.clickSchedule);
-          } else {
-            cal.on('clickSchedule', function(event) {
-              //var shedule = cal.getSchedule();
-              var schedule = event.schedule;
-              var element = cal.getSchedule(schedule.id, schedule.calendarId);
-              Shiny.setInputValue(el.id + '_schedule_click', element);
+              Shiny.setInputValue(el.id + "_schedules", element);
             });
           }
 
-          if (x.events.hasOwnProperty('beforeDeleteSchedule')) {
-            cal.on('beforeDeleteSchedule', x.events.beforeDeleteSchedule);
+          if (x.events.hasOwnProperty("clickSchedule")) {
+            cal.on("clickSchedule", x.events.clickSchedule);
+          } else {
+            cal.on("clickSchedule", function(event) {
+              //var shedule = cal.getSchedule();
+              var schedule = event.schedule;
+              var element = cal.getSchedule(schedule.id, schedule.calendarId);
+              Shiny.setInputValue(el.id + "_schedule_click", element);
+            });
           }
-          
-          if (x.events.hasOwnProperty('beforeUpdateSchedule')) {
-            cal.on('beforeUpdateSchedule', x.events.beforeUpdateSchedule);
+
+          if (x.events.hasOwnProperty("beforeDeleteSchedule")) {
+            cal.on("beforeDeleteSchedule", x.events.beforeDeleteSchedule);
           }
-          
-          if (x.events.hasOwnProperty('clickDayname')) {
-            cal.on('clickDayname', x.events.clickDayname);
+
+          if (x.events.hasOwnProperty("beforeUpdateSchedule")) {
+            cal.on("beforeUpdateSchedule", x.events.beforeUpdateSchedule);
           }
-          
-          if (x.events.hasOwnProperty('clickMorecalendar')) {
-            cal.on('clickMorecalendar', x.events.clickMorecalendar);
+
+          if (x.events.hasOwnProperty("clickDayname")) {
+            cal.on("clickDayname", x.events.clickDayname);
           }
-          
-          if (x.events.hasOwnProperty('clickTimezonesCollapseBtncalendar')) {
-            cal.on('clickTimezonesCollapseBtncalendar', x.events.clickTimezonesCollapseBtncalendar);
+
+          if (x.events.hasOwnProperty("clickMorecalendar")) {
+            cal.on("clickMorecalendar", x.events.clickMorecalendar);
           }
-          
+
+          if (x.events.hasOwnProperty("clickTimezonesCollapseBtncalendar")) {
+            cal.on(
+              "clickTimezonesCollapseBtncalendar",
+              x.events.clickTimezonesCollapseBtncalendar
+            );
+          }
         }
-        
-
       },
-      
-      getWidget: function(){
+
+      getWidget: function() {
         return cal;
       },
 
+      clickPrev: function(event) {
+        if (cal !== null) {
+          cal.prev();
+          renderRange.innerHTML =
+            dateToYMD(cal.getDateRangeStart()) +
+            " - " +
+            dateToYMD(cal.getDateRangeEnd());
+        }
+      },
+
+      clickNext: function(event) {
+        if (cal !== null) {
+          cal.next();
+          renderRange.innerHTML =
+            dateToYMD(cal.getDateRangeStart()) +
+            " - " +
+            dateToYMD(cal.getDateRangeEnd());
+        }
+      },
+
+      clickToday: function(event) {
+        if (cal !== null) {
+          cal.today();
+          renderRange.innerHTML =
+            dateToYMD(cal.getDateRangeStart()) +
+            " - " +
+            dateToYMD(cal.getDateRangeEnd());
+        }
+      },
+
       resize: function(width, height) {
-
         // TODO: code to re-render the widget with a new size
-
       }
-
     };
   }
 });
 
-
 // From Friss tuto (https://github.com/FrissAnalytics/shinyJsTutorials/blob/master/tutorials/tutorial_03.Rmd)
-function get_widget(id){
-  
+function get_widget(id) {
   // Get the HTMLWidgets object
   var htmlWidgetsObj = HTMLWidgets.find("#" + id);
-  
+
   // Use the getWidget method we created to get the underlying widget
-  var widgetObj ;
-  
-  if (typeof htmlWidgetsObj != 'undefined') {
+  var widgetObj;
+
+  if (typeof htmlWidgetsObj != "undefined") {
     widgetObj = htmlWidgetsObj.getWidget();
   }
 
-  return(widgetObj);
+  return widgetObj;
 }
-
 
 function dateToYMD(date) {
   var d = date.getDate();
   var m = date.getMonth() + 1; //Month from 0 to 11
   var y = date.getFullYear();
-  return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+  return "" + y + "-" + (m <= 9 ? "0" + m : m) + "-" + (d <= 9 ? "0" + d : d);
 }
-
-
 
 if (HTMLWidgets.shinyMode) {
-  Shiny.addCustomMessageHandler('proxy-tui-calendar-nav',
-    function(obj) {
-      var cal = get_widget(obj.id);
-      if (typeof cal != 'undefined') {
-        if (obj.data.where == 'prev') {
-          cal.prev();
-        }
-        if (obj.data.where == 'next') {
-          cal.next();
-        }
-        if (obj.data.where == 'today') {
-          cal.today();
-        }
-        if (obj.data.where == 'date') {
-          cal.setDate(obj.data.date);
-        }
-        Shiny.setInputValue(obj.id + '_dates', {
-          current: moment(cal.getDate()._date).format(),
-          start: moment(cal.getDateRangeStart()._date).format(),
-          end: moment(cal.getDateRangeEnd()._date).format()
-        });
+  Shiny.addCustomMessageHandler("proxy-tui-calendar-nav", function(obj) {
+    var cal = get_widget(obj.id);
+    if (typeof cal != "undefined") {
+      if (obj.data.where == "prev") {
+        cal.prev();
       }
+      if (obj.data.where == "next") {
+        cal.next();
+      }
+      if (obj.data.where == "today") {
+        cal.today();
+      }
+      if (obj.data.where == "date") {
+        cal.setDate(obj.data.date);
+      }
+      Shiny.setInputValue(obj.id + "_dates", {
+        current: moment(cal.getDate()._date).format(),
+        start: moment(cal.getDateRangeStart()._date).format(),
+        end: moment(cal.getDateRangeEnd()._date).format()
+      });
+    }
   });
-  Shiny.addCustomMessageHandler('proxy-tui-calendar-view',
-    function(obj) {
-      var cal = get_widget(obj.id);
-      if (typeof cal != 'undefined') {
-        cal.changeView(obj.data.view, true);
-      }
+  Shiny.addCustomMessageHandler("proxy-tui-calendar-view", function(obj) {
+    var cal = get_widget(obj.id);
+    if (typeof cal != "undefined") {
+      cal.changeView(obj.data.view, true);
+    }
   });
-  Shiny.addCustomMessageHandler('proxy-tui-calendar-create',
-    function(obj) {
-      var cal = get_widget(obj.id);
-      if (typeof cal != 'undefined') {
-        cal.createSchedules(obj.data.schedule);
-      }
+  Shiny.addCustomMessageHandler("proxy-tui-calendar-create", function(obj) {
+    var cal = get_widget(obj.id);
+    if (typeof cal != "undefined") {
+      cal.createSchedules(obj.data.schedule);
+    }
   });
-  Shiny.addCustomMessageHandler('proxy-tui-calendar-delete',
-    function(obj) {
-      var cal = get_widget(obj.id);
-      if (typeof cal != 'undefined') {
-        cal.deleteSchedule(obj.data.id, obj.data.calendarId);
-      }
+  Shiny.addCustomMessageHandler("proxy-tui-calendar-delete", function(obj) {
+    var cal = get_widget(obj.id);
+    if (typeof cal != "undefined") {
+      cal.deleteSchedule(obj.data.id, obj.data.calendarId);
+    }
   });
-  Shiny.addCustomMessageHandler('proxy-tui-calendar-update',
-    function(obj) {
-      var cal = get_widget(obj.id);
-      if (typeof cal != 'undefined') {
-        cal.updateSchedule(obj.data.id, obj.data.calendarId, obj.data.schedule);
-      }
+  Shiny.addCustomMessageHandler("proxy-tui-calendar-update", function(obj) {
+    var cal = get_widget(obj.id);
+    if (typeof cal != "undefined") {
+      cal.updateSchedule(obj.data.id, obj.data.calendarId, obj.data.schedule);
+    }
   });
-  Shiny.addCustomMessageHandler('proxy-tui-calendar-clear',
-    function(obj) {
-      var cal = get_widget(obj.id);
-      if (typeof cal != 'undefined') {
-        cal.clear(obj.data.immediately);
-      }
+  Shiny.addCustomMessageHandler("proxy-tui-calendar-clear", function(obj) {
+    var cal = get_widget(obj.id);
+    if (typeof cal != "undefined") {
+      cal.clear(obj.data.immediately);
+    }
   });
-  Shiny.addCustomMessageHandler('proxy-tui-calendar-options',
-    function(obj) {
-      var cal = get_widget(obj.id);
-      if (typeof cal != 'undefined') {
-        cal.setOptions(obj.data.options);
-      }
+  Shiny.addCustomMessageHandler("proxy-tui-calendar-options", function(obj) {
+    var cal = get_widget(obj.id);
+    if (typeof cal != "undefined") {
+      cal.setOptions(obj.data.options);
+    }
   });
 }
-
-
 
